0cfb7560b238f0181490ada3d8b0e2ad67c08ddb
WangKai1123 <wk563006423@163.com>
2025-02-09 21:49:06 +0800

    add gradio demo

--- gradio-dashboard.py
+++ gradio-dashboard.py
@@ -12,6 +12,17 @@ import gradio as gr
 load_dotenv()
 
 books = pd.read_csv("books_with_emotions.csv")
+books["large_thumbnail"] = books["thumbnail"] + "&fife=w800"
+books["large_thumbnail"] = np.where(
+    books["large_thumbnail"].isna(),
+    "cover-not-found.jpg",
+    books["large_thumbnail"],
+)
 
+raw_documents = TextLoader("tagged_descroption.txt").load()
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=0, chunk_overlap=0)
+documents = text_splitter.split_documents(raw_documents)
+db_books = Chroma.from_documents(documents,OpenAIEmbeddings)
 
+def retrieve_semantic_recommendations()
 
